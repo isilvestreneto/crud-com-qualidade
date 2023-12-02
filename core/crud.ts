866281e2fe0e3e5f80c676091778456e3015a4b3
Ivan <ivanildo.neto@capgemini.com>
2023-12-02 17:23:46 -0300

    2.3: update - uuid e granularidade de update de uma entidade

--- core/crud.ts
+++ core/crud.ts
@@ -1,45 +1,82 @@
-import fs from "fs";
-const DB_FILE_PATH = "./core/db";
+import fs from 'fs';
+import { v4 as uuid } from 'uuid';
 
-console.log("[CRUD]");
+const DB_FILE_PATH = './core/db';
+
+console.log('[CRUD]');
 
 interface Todo {
+    id: string;
     date: string;
     content: string;
     done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
     const todo: Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content: content,
-        done: false
+        done: false,
     };
 
-    const todos = [
-        read(),
-        todo,
-    ]
+    const todos = [...read(), todo];
 
     fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2));
-    return content;
+    return todo;
 }
 
 function read(): Array<Todo> {
-    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
-    const db = JSON.parse(dbString || "{}");
-    if (!db.todos) { // Fail fast validation
+    const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8');
+    const db = JSON.parse(dbString || '{}');
+    if (!db.todos) {
+        // Fail fast validation
         return [];
     }
     return db.todos;
 }
 
+function updateContentById(id: string, content: string): Todo {
+    return update(id, {
+        content,
+    });
+}
+
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+    let updatedTodo;
+    const todos = read();
+    todos.forEach(currentTodo => {
+        const isToUpdate = currentTodo.id === id;
+        if (isToUpdate) {
+            updatedTodo = Object.assign(currentTodo, partialTodo);
+        }
+    });
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2));
+
+    if (!updatedTodo) {
+        throw new Error('Please, provide another ID! :(');
+    }
+
+    return updatedTodo;
+}
+
 function CLEAR_DB() {
-    fs.writeFileSync(DB_FILE_PATH, "");
+    fs.writeFileSync(DB_FILE_PATH, '');
 }
 
 // [SIMULATION]
 CLEAR_DB();
-console.log(create("Hoje eu preciso gravar aulas!"));
-console.log(create("Segunda TODO"));
+create('Primeira TODO');
+const segundaTodo = create('Segunda TODO');
 console.log(read());
+const updatedContent = "Algo novo :)";
+console.log(`[UPDATE] ${segundaTodo.id}
+content: $${updatedContent}
+done: true`)
+updateContentById(segundaTodo.id, updatedContent);
+update(segundaTodo.id, {
+    done: true
+});
+console.log("After update:")
+console.log(read());
+
