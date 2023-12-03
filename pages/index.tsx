d056ff9d6f2115d31b8ea5083c729190989879e5
Ivan <ivanildo.neto@capgemini.com>
2023-12-03 16:13:13 -0300

    3.1: iniciando setup projeto react e nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to Next.JS!</h1>
+            <p>My first React App :)</p>
+        </div>
+    );
+}
+
+export default HomePage
